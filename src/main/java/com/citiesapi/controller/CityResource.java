660bf627fb9a30f960ff294573b75a19f8d0f98a
Anderson Monte <andersonsmont@gmail.com>
2022-04-09 10:14:48 -0300

    Refactoring

--- src/main/java/com/citiesapi/controller/CityResource.java
+++ src/main/java/com/citiesapi/controller/CityResource.java
@@ -1,15 +1,14 @@
 package com.citiesapi.controller;
 
-import java.util.List;
-
+import com.citiesapi.model.City;
+import com.citiesapi.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.citiesapi.model.City;
-import com.citiesapi.repository.CityRepository;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
